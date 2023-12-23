588c17ca33b8e885d725c1381f235b63f20f901f
Saksham <sakshamkohli6@gmail.com>
2023-12-22 22:03:30 -0500

    added css file

--- website/auth.py
+++ website/auth.py
@@ -30,6 +30,7 @@ def login():
 @login_required
 def logout():
     logout_user()
+    flash("You have been successfully logged out!")
     return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up',methods=['GET','POST'])
